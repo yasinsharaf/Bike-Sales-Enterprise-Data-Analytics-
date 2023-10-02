e6155c9146232af0b5b2d7243b9dd8fe534baace
yasinsharaf <100733146+yasinsharaf@users.noreply.github.com>
2023-10-01 21:15:48 -0700

    Delete Queries/daily_sales.sql

--- Queries/daily_sales.sql
+++
@@ -1,8 +0,0 @@
-select
-  DATE_NK,
-  round(sum(total_including_discount),2) as sales
-from
-  `dbt_yasinsharaf.fct_orders` orders
-  join `dbt_yasinsharaf.dim_date` dates on orders.ORDER_DATE_PK = dates.DATE_PK
-group by DATE_NK
-order by DATE_NK
